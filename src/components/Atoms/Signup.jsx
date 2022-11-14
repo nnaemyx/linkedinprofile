21da06e0e1414249ffeb6028076937dc775a802e
tezon <tezonteam@gmail.com>
2022-11-14 14:32:07 -0800

    new

--- src/components/Atoms/Signup.jsx
+++ src/components/Atoms/Signup.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Logo from "../../images/Logo (4).png";
 import Image from "../../images/Illustration (1).svg";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Signup = () => {
   return (
     <div className="pl-[10rem]  bg-white">
-      <div className=" flex justify-between">
+      <div className=" flex lg:flex-row flex-col justify-between">
         <div className="w-[35%] h-[120rem]">
           <img src={Logo} alt="" className="mb-10" />
           <div>
@@ -54,15 +55,33 @@ const Signup = () => {
               Jobs.
             </p>
           </div>
-            <div>
-            <p>
-                By creating an account, you agree to our Terms and have read and
-                acknowledge the Global Privacy Statement.
-                    </p>
-                    <input type="submit" />
+          <div className="mt-4 text-[#575757] ">
+            <p className="text-[13px] w-[35rem]  mb-6">
+              By creating an account, you agree to our{" "}
+              <span className="text-[#26619C] underline underline-offset-4">
+                Terms
+              </span>{" "}
+              and have read and acknowledge the{" "}
+              <span className="text-[#26619C] underline underline-offset-4">
+                Global Privacy Statement.
+              </span>
+            </p>
+            <div className="w-[7rem]">
+              <a
+                href="google.com"
+                className="px-6 gap-1 items-center py-3 flex rounded-md text-white text-[12.5px] bg-[#26619C]"
+              >
+                Sign Up
+                <span>
+                  <IoIosArrowForward size={15} />
+                </span>
+              </a>
             </div>
+            <p className="mt-4 text-[12px]">
+              Invisible reCAPTCHA by <span className=" underline underline-offset-4">Google Privacy Policy </span> and <span className="underline underline-offset-4">Terms of Use</span> 
+            </p>.
+          </div>
         </div>
-
 
         <div className="pr-8">
           <img src={Image} alt="" className="w-[401px]" />
